39b804d8c8c60fc189b6b9b3ac43b51ba4f010aa
Shalinee <shalineebhawnani90.com>
2020-05-11 20:19:11 +0530

    [shalinee] changed red to green

--- flask_demo.py
+++ flask_demo.py
@@ -5,7 +5,7 @@ app = Flask(__name__,)
 @app.route('/')
 def test():
     
-    return "<h1 style='color:red'>Hi hello flask, How are you color changed?</h1>"
+    return "<h1 style='color:green'>Hi hello flask, How are you color changed?</h1>"
 
 
 if __name__ == "__main__":
