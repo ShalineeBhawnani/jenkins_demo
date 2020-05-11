9987a75ad14cc38d3c872c9a6a2188e005c1a149
Shalinee <shalineebhawnani90.com>
2020-05-11 13:35:23 +0530

    second commit

---
+++ flask_demo.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__,)
+
+@app.route('/')
+def test():
+    
+    return "<h1 style='color:black'>Hi hello flask, How are you?</h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
